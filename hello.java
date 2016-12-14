62df6194cf6d3f9ee1fcad74cc89df2ac7f255e0
ghzhou <ghzhou@ctrip.com>
2016-12-14 10:02:24 +0800

    test property

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 
 public class HelloWorld{
+	private String userName;
+	private String sex;
+	
+	
 	public static void main(String []args){
 		System.out.println("Hello World , Hello China");
 		System.out.println("Hello World , Hello China");
