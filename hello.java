87a579a3eac954b1e11be2a31b2e0b489487b12c
ghzhou <ghzhou@ctrip.com>
2016-12-14 10:00:16 +0800

    add system .out.

--- hello.java
+++ hello.java
@@ -2,6 +2,9 @@
 public class HelloWorld{
 	public static void main(String []args){
 		System.out.println("Hello World , Hello China");
+		System.out.println("Hello World , Hello China");
+		System.out.println("Hello World , Hello China");
+		System.out.println("Hello World , Hello China");
 	}
 
 }
